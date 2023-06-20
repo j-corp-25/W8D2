965c0f516944fdb6823b014adf40b3cff018ee7c
XLG123 <xiaolin.guan123@gmail.com>
2023-06-20 15:21:24 -0400

    twosum completed

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -3,8 +3,7 @@ Array.prototype.uniq = function() {
   const new_array = [];
 
   this.forEach(element => {
-    if (!new_array.includes(element))
-    {
+    if (!new_array.includes(element)) {
       new_array.push(element);
     }
   });
@@ -13,3 +12,21 @@ Array.prototype.uniq = function() {
 };
 
 console.log([1, 2, 2, 3, 3, 3].uniq()) // => [1,2,3]
+
+Array.prototype.twoSum = function() {
+  const new_array = [];
+
+  this.forEach(element_1 => {
+    this.forEach(element_2 => {
+      const index_1 = this.indexOf(element_1);
+      const index_2 = this.indexOf(element_2);
+      if (element_1 + element_2 === 0 && index_1 < index_2) {
+        new_array.push([index_1, index_2]);
+      }
+    });
+  });
+
+  return new_array;
+};
+
+console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
